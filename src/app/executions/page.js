1678568e1d6060e6d6d4f959a5ec1a84a3f5e2fd
Manuel Grinspon <manugrinspon@gmail.com>
2026-06-02 21:52:34 -0300

    Correccion ejecuciones

--- src/app/executions/page.js
+++ src/app/executions/page.js
@@ -1,23 +1,48 @@
+"use client";
 import Link from 'next/link';
 import ExecutionsTable from './components/ExecutionsTable';
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 const EXECUTIONS_ENDPOINT = 'https://checklist-fwabdbgzf3cvf2br.brazilsouth-01.azurewebsites.net/api/executions';
 
 const LIMIT = 20;
 
 async function getExecutions(page) {
+  const token = localStorage.getItem("token");
   const url = `${EXECUTIONS_ENDPOINT}?page=${page}&limit=${LIMIT}`;
-  const res = await fetch(url, { next: { revalidate: 60 } });
+  const res = await fetch(url, {headers: { Authorization: `Bearer ${token}`,}, next: { revalidate: 60 } });
+   console.log("STATUS:", res.status);
+
+  const body = await res.text();
+  console.log("BODY:", body);
+
   if (!res.ok) {
-    throw new Error("No se pudo obtener el listado de ejecuciones");
+    throw new Error(`Error ${res.status}`);
   }
-  return res.json();
+
+  return JSON.parse(body);
 }
 
-export default async function ExecutionsPage({ searchParams }) {
-  const { page: pageParam } = await searchParams;
+export default function ExecutionsPage() {
+  const searchParams = useSearchParams();
+  const pageParam = searchParams.get("page");
   const page = Math.max(1, parseInt(pageParam) || 1);
-  const executions = await getExecutions(page);
+  const [executions, setExecutions] = useState([]);
+
+  useEffect(() => {
+    async function loadExecutions() {
+      try {
+        const data = await getExecutions(page);
+        setExecutions(data.data);
+        console.log("DATA:", data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    loadExecutions();
+  }, [page]);
   const hasNext = executions.length === LIMIT;
 
   return (
